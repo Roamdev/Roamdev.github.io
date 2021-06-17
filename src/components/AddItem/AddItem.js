aae97651b7ef12431951e6953a13e83074fb81c9
Roamdev <aroundthefog@gmail.com>
2021-06-17 19:12:24 +0400

    -

--- src/components/AddItem/AddItem.js
+++ src/components/AddItem/AddItem.js
@@ -4,43 +4,42 @@ import './AddItem.scss'
 
 export default class AddItem extends Component {
 
-  state = {
-    label: this.props.todos.label
-  }
+    state = {
+        label: this.props.todos.label
+    }
 
-  onLabelChange = (e) => {
-    this.setState ({
-      label: e.target.value
-    })
-  }
+    onLabelChange = (e) => {
+        this.setState({
+            label: e.target.value
+        })
+    }
 
-  onSubmit = (e) => {
-    e.preventDefault();
-    this.props.onAddItem(this.state.label);
-    this.setState({
-      label: ''
-    })
-  }
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.onAddItem(this.state.label);
+        this.setState({
+            label: ''
+        })
+    }
 
-  render() {
-    return (
-      <form
-        className="add-item"
-        onSubmit={this.onSubmit}>
+    render() {
+        return ( <
+            form className = "add-item"
+            onSubmit = { this.onSubmit } >
 
-        <input
-          type='text'
-          className='add-item--input form-control search-input'
-          onChange={this.onLabelChange}
-          placeholder='What needs to be done'
-          value={this.state.label}
-          />
+            <
+            input type = 'text'
+            className = 'add-item--input form-control search-input'
+            onChange = { this.onLabelChange }
+            placeholder = 'What needs to be done'
+            value = { this.state.label }
+            />
 
-        <button
-          className='btn btn-outline-secondary'
-          >Add Todo
-        </button>
-      </form>
-    )
-  }
-};
+            <
+            button className = 'btn btn-outline-secondary' >
+            Add Todo <
+            /button> <
+            /form>
+        )
+    }
+}
